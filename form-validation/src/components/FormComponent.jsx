78b0dc7b073633f29b02f896baf2a062e4b3dde6
Piti-Likit <Piti.nud@gmail.com>
2023-12-24 21:21:17 +0700

    Error Handling

--- form-validation/src/components/FormComponent.jsx
+++ form-validation/src/components/FormComponent.jsx
@@ -6,29 +6,61 @@ const FormComponent = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState ('')
     const [confirmedPassword, setConfirmedPassword] = useState ('')
+    
+    
+    const [errorUserName, setErrorUserName] = useState ('')
+    const [errorEmail, setErrorEmail] = useState ('')
+    const [errorPassword, setErrorPassword] = useState ('')
+    const [errorConfirmedPassword, setErrorConfirmedPassword]= useState ('')
+
+    const validateForm = (e) => {
+        e.preventDefault()
+        if (userName.length > 8){
+            setErrorUserName('')
+        }else{
+            setErrorUserName('Please enter more than 8 letters')
+        }
+        if (email.includes('@')){
+            setErrorEmail('')
+        }else{
+            setErrorEmail('Please enter email address')
+        }
+        if (password.length >8) {
+            setErrorPassword('')
+        }else{
+            setErrorPassword('Please enter 8-letter password')
+        }
+        if (confirmedPassword.length > 8) {
+            setErrorConfirmedPassword('')
+        }else{
+            setErrorConfirmedPassword('Please confirm password')
+        }
+    }
+    
+    
     return (
         <div className="container">
-            <form className="form">
+            <form className="form" onSubmit={validateForm}>
                 <h2>Registration Form</h2>
                 <div className="form-control">
                     <label>User</label>
                     <input type="text" value={userName} onChange={(e)=>setUserName(e.target.value)}/>
-                    <small>error message</small>
+                    <small>{errorUserName}</small>
                 </div>
                 <div className="form-control">
                     <label>Email</label>
                     <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-                    <small>error message</small>
+                    <small>{errorEmail}</small>
                 </div>
                 <div className="form-control">
                     <label>Password</label>
                     <input type="text" value={password} onChange={(e)=>setPassword(e.target.value)}/>
-                    <small>error message</small>
+                    <small>{errorPassword}</small>
                 </div>
                 <div className="form-control">
                     <label>Confirm Password</label>
                     <input type="text" value={confirmedPassword} onChange={(e)=>setConfirmedPassword(e.target.value)}/>
-                    <small>error message</small>
+                    <small>{errorConfirmedPassword}</small>
                 </div>
                 <button type="submit">Register</button>
             </form>
