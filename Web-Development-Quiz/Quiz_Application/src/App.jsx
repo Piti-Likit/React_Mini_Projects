be0b6cb615142d329c25fc9f7564aa4366678c7f
Piti-Likit <Piti.nud@gmail.com>
2023-12-26 16:52:41 +0700

    import createContex

--- Web-Development-Quiz/Quiz_Application/src/App.jsx
+++ Web-Development-Quiz/Quiz_Application/src/App.jsx
@@ -1,20 +1,25 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import { useState } from 'react'
+import { createContext } from 'react'
 import './App.css'
 import Menu from './Components/Menu'
 import Quiz from './Components/Quiz'
 import Score from './Components/Score'
 
+export const DataContext = createContext()
+
 function App() {
   const [appState, setAppState] = useState ("menu")
   return (
-    <div className='App'>
-      <h1>Web Development Quiz</h1>
-      {appState === "menu" && <Menu />}
-      {appState === "quiz" && <Quiz />}
-      {appState === "score" && <Score />}
-    </div>
+   <DataContext.Provider value={{appState, setAppState}}>
+     <div className='App'>
+        <h1>Web Development Quiz</h1>
+        {appState === "menu" && <Menu />}
+        {appState === "quiz" && <Quiz />}
+        {appState === "score" && <Score />}
+     </div>
+   </DataContext.Provider>
   )
 }
 
--- Web-Development-Quiz/Quiz_Application/src/Components/Menu.jsx
+++ Web-Development-Quiz/Quiz_Application/src/Components/Menu.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react"
+import { DataContext } from "../App"
+
 const Menu = () => {
+    const {setAppState} = useContext(DataContext)
     return(
     <div className="Menu">
         <h1>Menu Component</h1>
-
+        <button onClick={()=>{setAppState("quiz")}}>Start Quiz</button>
     </div>
     )
 }
