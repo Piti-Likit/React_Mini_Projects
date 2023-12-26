ad6f35d6b9aba9df04a0b8c8555b39e8158ff469
Piti-Likit <Piti.nud@gmail.com>
2023-12-26 22:37:08 +0700

    add choices

--- Web-Development-Quiz/Quiz_Application/src/Components/Quiz.jsx
+++ Web-Development-Quiz/Quiz_Application/src/Components/Quiz.jsx
@@ -4,15 +4,17 @@ import QuestionsData from "../data/QuestionsData"
 const Quiz = () => {
     // console.log(QuestionsData)
     const [current, setCurrent] = useState(0)
+    const [select, setSelect] = useState("")
     return(
     <div className="Quiz">
         <h1>{QuestionsData[current].question}</h1>
         <div className="Choices">
-            <button>{QuestionsData[current].A}</button>
-            <button>{QuestionsData[current].B}</button>
-            <button>{QuestionsData[current].C}</button>
-            <button>{QuestionsData[current].D}</button>
+            <button onClick={()=>setSelect("A")}>{QuestionsData[current].A}</button>
+            <button onClick={()=>setSelect("B")}>{QuestionsData[current].B}</button>
+            <button onClick={()=>setSelect("C")}>{QuestionsData[current].C}</button>
+            <button onClick={()=>setSelect("D")}>{QuestionsData[current].D}</button>
         </div>
+        <p>{`${current+1} / ${QuestionsData.length}`}</p>
     </div>
     )
 }
