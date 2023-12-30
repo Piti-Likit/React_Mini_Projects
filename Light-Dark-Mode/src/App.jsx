b1b526d13a7076c49f0a2c61029926dc750c6848
Piti-Likit <Piti.nud@gmail.com>
2023-12-30 13:21:41 +0700

    add React-switch && Context API

--- Light-Dark-Mode/src/App.jsx
+++ Light-Dark-Mode/src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
 import './App.css'
 import Title from './Components/Title'
 import Content from './Components/Content'
+import { createContext, useState } from 'react'
+
+export  const ThemeContext = createContext()
 
 function App() {
+  const [theme, setTheme] = useState("dark")
   return (
+    <ThemeContext.Provider value={{theme, setTheme}}> 
     <div>
         <Title />
         <Content />
     </div>
+    </ThemeContext.Provider>
   )
 }
 
--- Light-Dark-Mode/src/Components/Title.jsx
+++ Light-Dark-Mode/src/Components/Title.jsx
@@ -1,7 +1,17 @@
+import { useContext, useState } from 'react'
+import ReactSwitch from 'react-switch'
+import { ThemeContext } from '../App'
+
 const Title = () => {
+    const {theme, setTheme} = useContext(ThemeContext)
+
+    const toggleSwitch = (checked) => {
+        setTheme(checked)
+    }
     return(
         <header>
-            <span>Mode</span>
+            <span>Mode [{theme}]</span>
+            <ReactSwitch onChange={toggleSwitch} checked = {theme} />
         </header>
     )
 }
