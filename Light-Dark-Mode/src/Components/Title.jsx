f408fee292f467a93906b42411056779c15a9da9
Piti-Likit <Piti.nud@gmail.com>
2023-12-31 15:49:26 +0700

    add content's style and change picture with state

--- Light-Dark-Mode/src/Components/Title.jsx
+++ Light-Dark-Mode/src/Components/Title.jsx
@@ -11,9 +11,15 @@ const Title = () => {
         )
     }
     return(
-        <header className={theme==="dark"?"dark":"light"}>
+        <header className={theme === "dark"?"dark":"light"}>
             <span>Mode [{theme}]</span>
-            <ReactSwitch onChange={toggleSwitch} checked = {theme==="dark"} />
+            <ReactSwitch 
+            onChange={toggleSwitch} 
+            checked = {theme === "dark"} 
+            uncheckedIcon={false}
+            checkedIcon={false}
+            onColor='#ffa500'
+            />
         </header>
     )
 }
