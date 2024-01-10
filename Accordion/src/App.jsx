353c0aeefbfe803632d4fd533bf970f0eef70ab4
Piti-Likit <Piti.nud@gmail.com>
2024-01-10 20:22:50 +0700

    create the accordion

---
+++ Accordion/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+// import './App.css'
+import data from './data'
+
+function App() {
+  const [accordion, setAccordion] = useState (data)
+    return (
+      <main>
+        <div className='container'>
+         
+        </div>
+      </main>
+
+
+  )
+}
+
+export default App
