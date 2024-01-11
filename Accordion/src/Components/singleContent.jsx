6d3f7efaef94a2b7545b1555d01391c1e7e9eb77
Piti-Likit <Piti.nud@gmail.com>
2024-01-11 17:16:44 +0700

    Add useState to onClick to show info

--- Accordion/src/Components/singleContent.jsx
+++ Accordion/src/Components/singleContent.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react"
+
 const SingleContent = ({title, description}) => {
+    const [info, setInfo] = useState (false) 
     return (
         <article className="content">
             <h4>{title}</h4>
-            <button className="btn">Info</button>
-        <p>{description}</p>
+            <button className="btn" onClick={()=>setInfo(!info)}>Info</button>
+        {info && <p>{description}</p>}
         </article>
     )
 }
