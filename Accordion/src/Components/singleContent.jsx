77610a1667ea7710230996d1501eca5e7fc93bd8
Piti-Likit <Piti.nud@gmail.com>
2024-01-12 16:13:45 +0700

    Add CSS

--- Accordion/src/Components/singleContent.jsx
+++ Accordion/src/Components/singleContent.jsx
@@ -4,8 +4,10 @@ const SingleContent = ({title, description}) => {
     const [info, setInfo] = useState (false) 
     return (
         <article className="content">
-            <h4>{title}</h4>
-            <button className="btn" onClick={()=>setInfo(!info)}>Info</button>
+            <header>
+                <h4>{title}</h4>
+                <button className="btn" onClick={()=>setInfo(!info)}>Info</button>
+            </header>
         {info && <p>{description}</p>}
         </article>
     )
